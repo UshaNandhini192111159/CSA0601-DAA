fdfececab7b50e7c9b60192e3fdaa2ada1d93941
UshaNandhini192111159 <124120606+UshaNandhini192111159@users.noreply.github.com>
2023-08-29 20:52:51 +0530

    Create Reverse of a string .c

---
+++ Reverse of a string .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	printf("enter string:");
+	scanf("%s",s);
+	printf("reverse string:%s",strrev(s));	
+}
+
+OUTPUT:
+enter string:as|nr5y
+reverse string:y5rn|sa
+--------------------------------
+Process exited after 39.58 seconds with return value 22
+Press any key to continue . . .
